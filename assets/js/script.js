9c919dea1eb7b9bf9ddcb1a0c5e805fd52723919
Jairus Lai G. Balajadia <thephantomzx@gmail.com>
2025-12-12 22:00:14 +0800

    Delete assets directory
    maybe cuz its on folder it does not read

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-function replaceAll() {
-    let str1 = document.getElementById("txtInput").value;
-    console.log(str1);
-   
-
-    let newStr = str1.replaceAll(' ','');
-    document.getElementById("txtResult").innerHTML = newStr;
-
-}
